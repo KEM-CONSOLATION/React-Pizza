d2c7beeb8da8cdf3798683baf0065f737ea500b4
Kem-consolation <consolationlotachi@gmail.com>
2023-08-01 14:35:43 +0100

    Added the clear cart item

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
 import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart, getCart, getUser } from './cartSlice';
+import EmptyCart from './EmptyCart';
 
 const fakeCart = [
   {
@@ -31,28 +33,30 @@ const fakeCart = [
 ];
 
 function Cart() {
-  const username=useSelector(state=>state.user.username)
+  const username = useSelector(getUser);
 
-  const cart = fakeCart;
+  const cart = useSelector(getCart);
 
+  const dispatch = useDispatch();
+
+  if (!cart.length) return <EmptyCart />;
   return (
     <div className="px-4 py-3">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
-
       <h2 className="mt-7 text-xl font-semibold">Your cart, {username}</h2>
-
       <ul className="mt-3 divide-y divide-stone-200 border-b">
         {cart.map((item) => (
           <CartItem item={item} key={item.key} />
         ))}
       </ul>
-
       <div className="mt-6 space-x-2">
         <Button to="/order/new" type="primary">
           Order pizzas
         </Button>
 
-        <Button type="secondary">Clear cart</Button>
+        <Button type="secondary" onClick={() => dispatch(clearCart())}>
+          Clear cart
+        </Button>
       </div>
     </div>
   );
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,16 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  // cart:[],
-  cart: [
-    {
-      pizzaId: 12,
-      name: 'Meditarean',
-      quantity: 2,
-      unitiPrice: 16,
-      totalPrice: 32,
-    },
-  ],
+  cart: [],
+  // cart: [
+  //   {
+  //     pizzaId: 12,
+  //     name: 'Mediterranean',
+  //     quantity: 2,
+  //     unitPrice: 16,
+  //     totalPrice: 32,
+  //   },
+  // ],
 };
 
 const cartSlice = createSlice({
@@ -28,12 +28,12 @@ const cartSlice = createSlice({
     increaseItemQuantity(state, action) {
       const item = state.cart.find((item) => item.pizzaId === action.payload);
       item.quantity++;
-      item.totalPrice = item.quantity * item.unitiPrice;
+      item.totalPrice = item.quantity * item.unitPrice;
     },
     decreaseItemQuantity(state, action) {
       const item = state.cart.find((item) => item.pizzaId === action.payload);
       item.quantity--;
-      item.totalPrice = item.quantity * item.unitiPrice;
+      item.totalPrice = item.quantity * item.unitPrice;
     },
     clearCart(state) {
       state.cart = [];
@@ -50,3 +50,12 @@ export const {
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+export const getCart = (state) => state.cart.cart;
+
+export const getUser = (state) => state.user.username;
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
